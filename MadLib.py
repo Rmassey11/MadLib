22d43f3afef262e0eee9eff3544255466345b932
Rmassey11 <ronmassey11@gmail.com>
2023-03-25 10:57:21 -0400

    Create MadLib.py
    Created basic MadLib

---
+++ MadLib.py
@@ -0,0 +1,9 @@
+adj = input(" Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+noun = input("Noun: ")
+
+madLib = f"Learning to code is so {adj}! It makes me feel like I can {verb1}. \
+I can't wait to be able to {verb2} like a {noun}!"
+
+print(madLib)
